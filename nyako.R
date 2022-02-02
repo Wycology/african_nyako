a3156952a506f614615333a632f8eaf74fe8cd37
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-02-02 20:04:04 +0300

    main

--- nyako.R
+++ nyako.R
@@ -14,7 +14,7 @@ names(df) <- 'text'
 df <- df %>% mutate(divs = 1:nrow(df)) %>% 
   filter(divs%%2 == 0)
 
-nyako <- df
+nyako <- df # Assigning the dataframe to another object 'nyako'
 
 head(nyako)
 
