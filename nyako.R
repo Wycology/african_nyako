7b47fbcf96617928959ce859132731f22bc68ca1
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-02-08 19:50:18 +0300

    c

--- nyako.R
+++ nyako.R
@@ -14,7 +14,7 @@ names(df) <- 'text'
 df <- df %>% mutate(divs = 1:nrow(df)) %>% 
   filter(divs%%2 == 0)
 
-nyako <- df # Assigning the dataframe to another object 'nyako'
+nyako <- df # Assigning the dataframe to another object 'nyako
 
 head(nyako) # Checking the first six rows of the dataset
 
